6471af7db97b4f30ac3962e05652ca024d50744b
bsolos <me@bsolos.heliohost.us>
2023-02-10 23:16:28 +0200

    Finish proof of concept implementation

--- src/vega3_workaround.cpp
+++ src/vega3_workaround.cpp
@@ -3,14 +3,21 @@
 #include <mutex>
 #include <vector>
 #include <algorithm>
+#include <libdrm/amdgpu.h>
+#include <fcntl.h>
 
 #include "vega3_workaround.h"
 
-#define SAMPLE_BUF_SIZE 128
-#define SAMPLING_RATE 256
+#define SAMPLE_BUF_SIZE 256
+#define SAMPLING_RATE 512
+
+#define GRBM_STATUS 0x8010
+#define UTIL_BIT (1U<<31)
 
 void samplerMain();
-bool bit31NotZero();
+bool utilBitNotZero();
+
+amdgpu_device_handle amdgpu_dev;
 
 bool sampler_thread_running = false;
 std::thread sampler_thread;
@@ -22,7 +29,12 @@ bool v3w::samplerThreadRunning() {
 }
 
 void v3w::startSamplerThread() {
+    int fd = open("/dev/dri/card0", O_RDWR); // horrible hardcode
+    uint32_t drm_minor, drm_major; 
+    amdgpu_device_initialize(fd, &drm_minor, &drm_major, &amdgpu_dev);
+
     sampler_thread = std::thread(samplerMain);
+    sampler_thread.detach();
     sampler_thread_running = true;
 }
 
@@ -43,14 +55,16 @@ void samplerMain() {
         sample_buf_mutex.lock();
 
         std::rotate(sample_buf.begin(), sample_buf.begin() + 1, sample_buf.end());
-        sample_buf[SAMPLE_BUF_SIZE - 1] = bit31NotZero();
+        sample_buf[SAMPLE_BUF_SIZE - 1] = utilBitNotZero();
 
         sample_buf_mutex.unlock();
         std::this_thread::sleep_for(std::chrono::microseconds(1000000 / SAMPLING_RATE));
     }
 }
 
-bool bit31NotZero() {
-    // TODO: Implement
-    return false;
+bool utilBitNotZero() {
+    uint32_t registers;
+    amdgpu_read_mm_registers(amdgpu_dev, GRBM_STATUS / 4, 1, 0xffffffff, 0, &registers);
+
+    return (registers & UTIL_BIT) != 0;
 }
